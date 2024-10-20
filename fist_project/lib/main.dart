46f43f0d6442cfd4667570b228340a2c57731c0d
10Robert <robertlucasmtz124@gmail.com>
2024-10-20 20:31:34 -0300

    understading class | bulding custom widgets

--- fist_project/lib/main.dart
+++ fist_project/lib/main.dart
@@ -4,10 +4,20 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
+        body: GradientContainer(),
+      ),
+    )
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
-              colors: [Color.fromRGBO(113, 64, 226, 0.877), Color.fromRGBO(73, 200, 200, 0.498)],
+              colors: [Color.fromRGBO(113, 64, 226, 0.877),
+               Color.fromRGBO(73, 200, 200, 0.498)],
               begin: Alignment.center,
               end: Alignment.topLeft,
             )
@@ -18,8 +28,7 @@ void main() {
               fontSize: 28.5,
             )),
           ),
-        ),
-      ),
-    )
-  );
+        );
+  }
+
 }
